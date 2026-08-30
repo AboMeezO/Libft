1e90900c21cd7c2c6269977849dfb2d2c2353d60
AboMeezO <abomeezo2@gmail.com>
2026-08-30 23:04:04 +0300

    ft_memset.c is done

---
+++ ft_memset.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    unsigned char *p = (unsigned char *)s;
+    size_t i;
+
+    for (i = 0; i < n; i++)
+        p[i] = (unsigned char)c;
+    return (s);
+}
+/*
+#include <stdio.h>
+int main ()
+{
+    int index = 0;
+    char *str[20];
+    char *result = ft_memset(str, 69,15);
+    while (result[index])
+    {
+        index++;
+        printf("%d%c\n", index, result[index]);
+    }
+    printf("\n");
+    return (0);
+}
+*/
