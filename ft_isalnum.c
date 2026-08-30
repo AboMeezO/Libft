45ed329394ce824bc81fef178eda1fdd650aacc7
AboMeezO <abomeezo2@gmail.com>
2026-08-30 21:55:47 +0300

    lovely

---
+++ ft_isalnum.c
@@ -0,0 +1,21 @@
+int isalnum(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+        return (1);
+    return (0);
+}
+
+///*
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        return (1);
+    if (isalnum(argv[1][0]))
+        printf("The character is alphanumeric.\n");
+    else
+        printf("The character is not alphanumeric.\n");
+    return (0);
+}
+//*/
---
+++ ft_strlen.c
@@ -0,0 +1,8 @@
+size_t	strlen(const char *s)
+{
+    size_t index;
+    index = 0;
+    while (s[index])
+        index++;
+    return (index);
+}
