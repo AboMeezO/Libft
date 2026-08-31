33c0bbfd2a3dda5e37ac33fb2bf65e090f365806
AboMeezO <abomeezo2@gmail.com>
2026-08-31 16:37:56 +0300

    feet(add): ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+	size_t index;
+
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	index = 0;
+	while (index < n && str1[index] == str2[index])
+		index++;
+	if (index == n)
+		return (0);
+	if (str1[index] > str2[index])
+		return (1);
+	return (-1);
+}
