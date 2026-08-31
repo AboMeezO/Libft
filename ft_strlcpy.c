3929b258f8de9fe7fd4db53e39933b5bbf6f23ae
AboMeezO <abomeezo2@gmail.com>
2026-08-31 12:44:11 +0300

    feat(add): added ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	index;
+
+	index = 0;
+	if (dstsize > 0)
+	{
+		while (src[index] && index < dstsize - 1)
+		{
+			dst[index] = src[index];
+			index++;
+		}
+		dst[index] = '\0';
+	}
+	while (src[index])
+		index++;
+	return (index);
+}
