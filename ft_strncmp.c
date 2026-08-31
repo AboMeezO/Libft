f94d908064bff941cd6bfa50da02908e6c75ad0c
AboMeezO <abomeezo2@gmail.com>
2026-08-31 15:42:07 +0300

    feet(add): ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	index;
+
+	index = 0;
+	while (index < n && s1[index] && s1[index] == s2[index])
+		index++;
+	if (index == n || s1[index] == s2[index])
+		return (0);
+	if (s1[index] > s2[index])
+		return (1);
+	return (-1);
+}
