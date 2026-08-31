315eac33405f5ddd45e717181de4a496d7f4eb5b
AboMeezO <abomeezo2@gmail.com>
2026-08-31 16:24:53 +0300

    feet(add): ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+
+void    *ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*str;
+	size_t index;
+
+	str = (const unsigned char *)s;
+	index = 0;
+	while (index < n)
+	{
+		if (str[index] == (unsigned char)c)
+			return ((void *)&str[index]);
+		index++;
+	}
+	return (NULL);
+}
