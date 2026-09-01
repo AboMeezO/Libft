d6cf24146a35f3a9affc7752d2e6a0e14ccd25d9
AboMeezO <abomeezo2@gmail.com>
2026-09-01 21:35:43 +0300

    fix(ft_memset.c): removed commented main function

--- ft_memset.c
+++ ft_memset.c
@@ -14,21 +14,3 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return (s);
 }
-/*
-#include <stdio.h>
-int	main()
-{
-	int index;
-	index = 0;
-	char *str[20];
-	char *result;
-	result = ft_memset(str, 69,15);
-	while (result[index])
-	{
-		index++;
-		printf("%d%c\n", index, result[index]);
-	}
-	printf("\n");
-	return (0);
-}
-*/
