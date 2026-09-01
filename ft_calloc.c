c2ca310923f72ca586a2175d6d34e5b46f43d499
AboMeezO <abomeezo2@gmail.com>
2026-09-01 14:18:00 +0300

    feet(add): ft_calloc.c. part 1 isn't finished yet, one function remains

---
+++ ft_calloc.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+void	*ft_calloc(size_t n, size_t size)
+{
+	size_t	index;
+	void	*ptr;
+	size_t	total_size;
+
+    if (n != 0 && size > (size_t)-1 / n)
+        return (NULL);
+	total_size = n * size;
+	ptr = malloc(total_size);
+	if (ptr)
+	{
+		for (index = 0; index < total_size; index++)
+			((char *)ptr)[index] = 0;
+	}
+	return (ptr);
+}
