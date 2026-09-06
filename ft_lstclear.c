da8e92da364f1a845301db88828b0528f79beffa
mohammah <mohammah@c2r6s7.irbid.42.school>
2026-09-06 20:35:22 +0300

    feet(add): ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/06 20:27:46 by mohammah          #+#    #+#             */
+/*   Updated: 2026/09/06 20:34:55 by mohammah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_list.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*temp;
+
+	current = *lst;
+	while (current != NULL)
+	{
+		temp = current->next;
+		del(current->content);
+		free(current);
+		current = temp;
+	}
+	*lst = NULL;
+}
