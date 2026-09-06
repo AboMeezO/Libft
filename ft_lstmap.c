24a1193799e23931769fa2d45abe01b127ba47ac
mohammah <mohammah@c2r6s7.irbid.42.school>
2026-09-06 21:04:40 +0300

    feet(add): ft_lstmap.c

---
+++ ft_lstmap.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/06 20:43:32 by mohammah          #+#    #+#             */
+/*   Updated: 2026/09/06 21:01:11 by mohammah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stddef.h>
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),
+		void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*content;
+
+	new_list = NULL;
+	while (lst != NULL)
+	{
+		content = f(lst->content);
+		new_node = ft_lstnew(content);
+		if (new_node == NULL)
+		{
+			del(content);
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
