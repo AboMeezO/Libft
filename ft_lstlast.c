9611a1737f2fd1bce391644656d1f54bb050caf4
mohammah <mohammah@c2r6s7.irbid.42.school>
2026-09-06 20:18:33 +0300

    feet(add): ft_lstlast.c

---
+++ ft_lstlast.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/06 20:14:03 by mohammah          #+#    #+#             */
+/*   Updated: 2026/09/06 20:18:10 by mohammah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stddef.h>
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*current;
+
+	current = lst;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	return (current);
+}
