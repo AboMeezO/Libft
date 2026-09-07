9db47a5fafdce51cfd6dfb8484c5c6405c62b7fe
AboMeezO <abomeezo2@gmail.com>
2026-09-08 01:04:33 +0300

    made an include guard

--- ft_list.h
+++ ft_list.h
@@ -6,12 +6,17 @@
 /*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/06 12:41:36 by mohammah          #+#    #+#             */
-/*   Updated: 2026/09/08 00:29:00 by mohammah         ###   ########.fr       */
+/*   Updated: 2026/09/08 01:02:44 by mohammah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
 
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
+#endif
