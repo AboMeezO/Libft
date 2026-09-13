7db265985b0ac3d1b6b63eb03f47324406d300c2
Dead Signal <abomeezo2@gmail.com>
2026-09-13 12:51:42 +0300

    add strrchr tester

---
+++ tester/Part1/strrchr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "../../libft.h"
+#include "../tester.h"
+
+void	test_strrchr(void)
+{
+	char	*str;
+
+	str = "hello";
+	printf("\nft_strrchr\n");
+	print_case("last occurrence", ft_strrchr(str, 'l') == str + 3);
+	print_case("first char", ft_strrchr(str, 'h') == str);
+	print_case("missing char", ft_strrchr(str, 'z') == NULL);
+	print_case("null terminator", ft_strrchr(str, '\0') == str + 5);
+	print_case("repeated char", ft_strrchr("banana", 'a') == "banana" + 5);
+}
