2ea3b6566a98f2bbbe36630fe578801ef8ad912f
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:21:38 +0300

    show putchar_fd test input and output

--- tester/Part2/putchar_fd.c
+++ tester/Part2/putchar_fd.c
@@ -4,9 +4,10 @@
 
 void	test_putchar_fd(void)
 {
-	printf("\nft_putchar_fd\n");
-	printf("  expected: A\n  output:   ");
+	printf("Test 1\n");
+	printf("  input:    'A', fd=1\n");
+	printf("  expected: A\n");
+	printf("  actual:   ");
 	ft_putchar_fd('A', 1);
-	printf("\n");
-	print_case("manual output check", 1);
+	printf("\n\n");
 }
