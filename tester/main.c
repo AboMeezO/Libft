c1c4940c4bf80564ec4e86e5167a1a800b7bfc8b
Dead Signal <abomeezo2@gmail.com>
2026-09-13 12:32:09 +0300

    add string and memory tests to tester

--- tester/main.c
+++ tester/main.c
@@ -2,30 +2,57 @@
 #include <string.h>
 #include "tester.h"
 
-static int	run_test(char *name)
-{
-	int	result;
+static const t_test_case g_tests[] = {
+	{"ft_isalpha", test_isalpha},
+	{"ft_isdigit", test_isdigit},
+	{"ft_isalnum", test_isalnum},
+	{"ft_isascii", test_isascii},
+	{"ft_isprint", test_isprint},
+	{"ft_toupper", test_toupper},
+	{"ft_tolower", test_tolower},
+	{"ft_strlen", test_strlen},
+	{"ft_strchr", test_strchr},
+	{"ft_strrchr", test_strrchr},
+	{"ft_strncmp", test_strncmp},
+	{"ft_memset", test_memset},
+	{"ft_bzero", test_bzero},
+	{"ft_memcpy", test_memcpy},
+	{"ft_memmove", test_memmove},
+	{"ft_memchr", test_memchr},
+	{"ft_memcmp", test_memcmp},
+};
 
-	result = -1;
-	if (strcmp(name, "ft_isalpha") == 0)
-		result = test_isalpha();
-	else if (strcmp(name, "ft_isdigit") == 0)
-		result = test_isdigit();
-	else if (strcmp(name, "ft_isalnum") == 0)
-		result = test_isalnum();
-	else if (strcmp(name, "ft_isascii") == 0)
-		result = test_isascii();
-	else if (strcmp(name, "ft_isprint") == 0)
-		result = test_isprint();
-	else if (strcmp(name, "ft_toupper") == 0)
-		result = test_toupper();
-	else if (strcmp(name, "ft_tolower") == 0)
-		result = test_tolower();
-	else
+static size_t	test_count(void)
+{
+	return (sizeof(g_tests) / sizeof(g_tests[0]));
+}
+
+static const t_test_case	*find_test(const char *name)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < test_count())
+	{
+		if (strcmp(name, g_tests[index].name) == 0)
+			return (&g_tests[index]);
+		index++;
+	}
+	return (NULL);
+}
+
+static int	run_test(const char *name)
+{
+	const t_test_case	*test;
+	int					result;
+
+	test = find_test(name);
+	if (test == NULL)
 	{
 		printf("? %s: test not found\n", name);
 		return (0);
 	}
+	result = test->run();
 	if (result == 1)
 		printf("OK  %s\n", name);
 	else
