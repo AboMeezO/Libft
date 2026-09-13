5d5422d124a6a8109efec3b7e7641e1b9e1c9770
Dead Signal <abomeezo2@gmail.com>
2026-09-13 12:55:54 +0300

    fix lstclear tester

--- tester/Part3/lstclear.c
+++ tester/Part3/lstclear.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../../libft.h"
 #include "../tester.h"
 
+static void	delete_content(void *content)
+{
+	free(content);
+}
+
 void	test_lstclear(void)
 {
-	t_list	a;
-	t_list	b;
+	char	*first_content;
+	char	*second_content;
+	t_list	first;
+	t_list	second;
 	t_list	*list;
 
 	printf("\nft_lstclear\n");
-	a.next = &b;
-	b.next = NULL;
-	list = &a;
-	ft_lstclear(&list, NULL);
+	first_content = malloc(2);
+	second_content = malloc(2);
+	first.content = first_content;
+	second.content = second_content;
+	first.next = &second;
+	second.next = NULL;
+	list = &first;
+	ft_lstclear(&list, delete_content);
 	print_case("list becomes NULL", list == NULL);
 }
