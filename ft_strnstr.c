9800e423b1fab995096777f9ee4429c005ad36e2
mohammah <mohammah@c2r6s7.irbid.42.school>
2026-09-13 10:14:04 +0300

    fix(strnstr): wrong prototype were used

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,30 +6,30 @@
 /*   By: mohammah <mohammah@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/01 23:23:45 by mohammah          #+#    #+#             */
-/*   Updated: 2026/09/08 00:29:24 by mohammah         ###   ########.fr       */
+/*   Updated: 2026/09/13 10:13:17 by mohammah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	outer_index;
 	size_t	inner_index;
 
 	outer_index = 0;
-	if (to_find[0] == '\0')
-		return ((char *) str);
-	while (str[outer_index] && outer_index < len)
+	if (little[0] == '\0')
+		return ((char *) big);
+	while (big[outer_index] && outer_index < len)
 	{
 		inner_index = 0;
-		while (str[outer_index + inner_index]
-			&& to_find[inner_index]
+		while (big[outer_index + inner_index]
+			&& little[inner_index]
 			&& outer_index + inner_index < len
-			&& str[outer_index + inner_index] == to_find[inner_index])
+			&& big[outer_index + inner_index] == little[inner_index])
 			inner_index++;
-		if (to_find[inner_index] == '\0')
-			return ((char *) & str[outer_index]);
+		if (little[inner_index] == '\0')
+			return ((char *) & big[outer_index]);
 		outer_index++;
 	}
 	return (NULL);
