878c97c26fc546bfda8e92e3d4c3d517159b0db1
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:17:19 +0300

    show memcpy test inputs and outputs

--- tester/Part1/memcpy.c
+++ tester/Part1/memcpy.c
@@ -8,10 +8,14 @@ void	test_memcpy(void)
 	char	src[] = "hello";
 	char	dst[6];
 
-	printf("\nft_memcpy\n");
+	printf("Test 1\n");
+	printf("  input:    dst, \"hello\", 6\n");
 	ft_memcpy(dst, src, 6);
-	print_case("copy string", strcmp(dst, "hello") == 0);
-	print_case("copies terminator", dst[5] == '\0');
+	printf("  expected: \"hello\"\n");
+	printf("  actual:   \"%s\"\n\n", dst);
+	printf("Test 2\n");
+	printf("  input:    dst, \"abcde\", 0\n");
 	ft_memcpy(dst, "abcde", 0);
-	print_case("zero length", strcmp(dst, "hello") == 0);
+	printf("  expected: \"hello\"\n");
+	printf("  actual:   \"%s\"\n\n", dst);
 }
