53cc883fd8f0b2b18ab41a1d8db310828823d8de
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:19:24 +0300

    show lstadd_front test inputs and outputs

--- tester/Part3/lstadd_front.c
+++ tester/Part3/lstadd_front.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../../libft.h"
 #include "../tester.h"
 
 void	test_lstadd_front(void)
 {
-	t_list	first;
-	t_list	second;
-	t_list	*list;
+	t_list	*first;
+	t_list	*second;
 
-	printf("\nft_lstadd_front\n");
-	first.next = NULL;
-	second.next = NULL;
-	list = &first;
-	ft_lstadd_front(&list, &second);
-	print_case("new head", list == &second);
-	print_case("old head follows", list->next == &first);
+	first = ft_lstnew("first");
+	second = ft_lstnew("second");
+	printf("Test 1\n");
+	printf("  input:    list=\"second\", new=\"first\"\n");
+	ft_lstadd_front(&second, first);
+	printf("  expected: first -> second\n");
+	printf("  actual:   %s -> %s\n\n", (char *)second->content, (char *)second->next->content);
+	free(first);
+	free(second);
 }
