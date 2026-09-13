62fc000da34b75dea64cafa09f6564e828ade214
Dead Signal <abomeezo2@gmail.com>
2026-09-13 12:53:06 +0300

    fix strrchr repeated test

--- tester/Part1/strrchr.c
+++ tester/Part1/strrchr.c
@@ -5,12 +5,14 @@
 void	test_strrchr(void)
 {
 	char	*str;
+	char	*banana;
 
 	str = "hello";
+	banana = "banana";
 	printf("\nft_strrchr\n");
 	print_case("last occurrence", ft_strrchr(str, 'l') == str + 3);
 	print_case("first char", ft_strrchr(str, 'h') == str);
 	print_case("missing char", ft_strrchr(str, 'z') == NULL);
 	print_case("null terminator", ft_strrchr(str, '\0') == str + 5);
-	print_case("repeated char", ft_strrchr("banana", 'a') == "banana" + 5);
+	print_case("repeated char", ft_strrchr(banana, 'a') == banana + 5);
 }
