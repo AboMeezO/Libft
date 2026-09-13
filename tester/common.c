163e52dd1aee74e20ed979668b8966d77daaf7d6
Dead Signal <abomeezo2@gmail.com>
2026-09-13 13:41:14 +0300

    Improve tester output helpers

--- tester/common.c
+++ tester/common.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include "tester.h"
 
-void	print_case(const char *name, int passed)
+void	print_function(const char *name)
 {
-	printf("  %-24s %s\n", name, passed ? "OK" : "KO");
+	printf("\n========================================\n");
+	printf("%s\n", name);
+	printf("========================================\n\n");
+}
+
+void	print_int_case(int number, const char *input, int expected, int actual)
+{
+	printf("Test %d\n", number);
+	printf("  input:    %s\n", input);
+	printf("  expected: %d\n", expected);
+	printf("  actual:   %d\n\n", actual);
+}
+
+void	print_str_case(int number, const char *input, const char *expected,
+	const char *actual)
+{
+	printf("Test %d\n", number);
+	printf("  input:    %s\n", input);
+	printf("  expected: %s\n", expected);
+	printf("  actual:   %s\n\n", actual);
 }
