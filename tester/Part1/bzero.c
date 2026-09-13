14514093bf54ca994bd2072dda325d2bd48d7bb8
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:17:00 +0300

    show bzero test inputs and outputs

--- tester/Part1/bzero.c
+++ tester/Part1/bzero.c
@@ -11,10 +11,15 @@ void	test_bzero(void)
 	buffer[2] = 'c';
 	buffer[3] = 'd';
 	buffer[4] = 'e';
-	printf("\nft_bzero\n");
+	printf("Test 1\n");
+	printf("  input:    \"abcde\", 5\n");
 	ft_bzero(buffer, 5);
-	print_case("all bytes zero", buffer[0] == 0 && buffer[4] == 0);
+	printf("  expected: 00 00 00 00 00\n");
+	printf("  actual:   %d %d %d %d %d\n\n", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4]);
 	buffer[0] = 'x';
+	printf("Test 2\n");
+	printf("  input:    \"x\", 0\n");
 	ft_bzero(buffer, 0);
-	print_case("zero length", buffer[0] == 'x');
+	printf("  expected: x unchanged\n");
+	printf("  actual:   %c\n\n", buffer[0]);
 }
