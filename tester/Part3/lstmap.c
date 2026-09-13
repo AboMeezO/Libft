838be91dc61b7cbf327980098923129fd9dc8bff
Dead Signal <abomeezo2@gmail.com>
2026-09-13 12:55:35 +0300

    add lstmap tester

---
+++ tester/Part3/lstmap.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../libft.h"
+#include "../tester.h"
+
+static void	*duplicate_content(void *content)
+{
+	return (strdup((char *)content));
+}
+
+static void	delete_content(void *content)
+{
+	free(content);
+}
+
+void	test_lstmap(void)
+{
+	t_list	first;
+	t_list	second;
+	t_list	*result;
+
+	printf("\nft_lstmap\n");
+	first.content = "hello";
+	first.next = &second;
+	second.content = "world";
+	second.next = NULL;
+	result = ft_lstmap(&first, duplicate_content, delete_content);
+	print_case("two nodes", result && result->next
+		&& strcmp(result->content, "hello") == 0
+		&& strcmp(result->next->content, "world") == 0);
+	ft_lstclear(&result, delete_content);
+}
