ac455b9ed2dca4b67167064d8af505ab3535474c
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:22:02 +0300

    show lstdelone test inputs and outputs

--- tester/Part3/lstdelone.c
+++ tester/Part3/lstdelone.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "../../libft.h"
 #include "../tester.h"
 
@@ -14,11 +13,19 @@ void	test_lstdelone(void)
 	char	*content;
 	t_list	*node;
 
-	printf("\nft_lstdelone\n");
 	content = malloc(6);
-	if (content)
-		memcpy(content, "hello", 6);
+	if (content == NULL)
+		return ;
+	content[0] = 'h';
+	content[1] = 'e';
+	content[2] = 'l';
+	content[3] = 'l';
+	content[4] = 'o';
+	content[5] = '\0';
 	node = ft_lstnew(content);
+	printf("Test 1\n");
+	printf("  input:    node with content=\"hello\"\n");
+	printf("  expected: node content is deleted\n");
 	ft_lstdelone(node, delete_content);
-	print_case("node deleted", 1);
+	printf("  actual:   node was passed to delete function\n\n");
 }
