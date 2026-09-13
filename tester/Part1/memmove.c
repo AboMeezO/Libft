3b23d8cf48c8f713326f92af922837feb26a42fd
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:17:25 +0300

    show memmove test inputs and outputs

--- tester/Part1/memmove.c
+++ tester/Part1/memmove.c
@@ -7,12 +7,17 @@ void	test_memmove(void)
 {
 	char	buffer[7];
 
-	printf("\nft_memmove\n");
 	strcpy(buffer, "123456");
+	printf("Test 1\n");
+	printf("  input:    \"123456\", move buffer to buffer + 2, 4\n");
 	ft_memmove(buffer + 2, buffer, 4);
-	print_case("overlap forward", strcmp(buffer, "121234") == 0);
+	printf("  expected: \"121234\"\n");
+	printf("  actual:   \"%s\"\n\n", buffer);
 	strcpy(buffer, "123456");
+	printf("Test 2\n");
+	printf("  input:    \"123456\", move buffer + 2 to buffer, 4\n");
 	ft_memmove(buffer, buffer + 2, 4);
 	buffer[4] = '\0';
-	print_case("overlap backward", strcmp(buffer, "3456") == 0);
+	printf("  expected: \"3456\"\n");
+	printf("  actual:   \"%s\"\n\n", buffer);
 }
