7dbee17c506961fc51cf5d6d7a72349aa42caadd
Dead Signal <abomeezo2@gmail.com>
2026-09-13 14:17:33 +0300

    show memset test inputs and outputs

--- tester/Part1/memset.c
+++ tester/Part1/memset.c
@@ -6,11 +6,15 @@ void	test_memset(void)
 {
 	char	buffer[6];
 
-	printf("\nft_memset\n");
-	ft_memset(buffer, 'a', 5);
 	buffer[5] = '\0';
-	print_case("fill bytes", buffer[0] == 'a' && buffer[4] == 'a');
-	print_case("terminator untouched", buffer[5] == '\0');
-	ft_memset(buffer, 0, 0);
-	print_case("zero length", buffer[0] == 'a');
+	printf("Test 1\n");
+	printf("  input:    buffer, 'a', 5\n");
+	ft_memset(buffer, 'a', 5);
+	printf("  expected: \"aaaaa\"\n");
+	printf("  actual:   \"%s\"\n\n", buffer);
+	printf("Test 2\n");
+	printf("  input:    buffer, 'b', 0\n");
+	ft_memset(buffer, 'b', 0);
+	printf("  expected: \"aaaaa\"\n");
+	printf("  actual:   \"%s\"\n\n", buffer);
 }
